992a2fdc7ed7e54dc85b570d19d363968dd1fbfa
Vinicius Mendes <vmendes@vmendes.xyz>
2025-05-19 14:33:27 -0300

    Remove dependência GDAL para corrigir CI

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qgis_interpolador",
+    version="0.1.0",
+    description="Ferramenta de interpolação espacial com IDW e Krigagem para uso com QGIS ou Python puro",
+    author="Vinicius Mendes",
+    author_email="vmendes@vmendes.xyz",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pykrige",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
