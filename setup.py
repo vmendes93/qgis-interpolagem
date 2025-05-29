c93aee5403a86e9b6240655528dc3042a90607c6
Vinicius Mendes <vmendes@vmendes.xyz>
2025-05-29 16:51:10 -0300

    Correções nos testes

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     install_requires=[
         "numpy",
         "matplotlib",
+        "scipy",
+        "matplotlib"
         "pykrige",
     ],
     classifiers=[
@@ -18,5 +20,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
