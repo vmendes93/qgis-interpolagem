875edd7a4f321c8ef5862f49cbcae0a3d301344f
Vinicius Mendes <vmendes@vmendes.xyz>
2025-05-28 21:41:49 -0300

    style(docs): aplica corretamente o tema customizado à documentação HTML

--- docs_theme/config.mjs
+++ docs_theme/config.mjs
@@ -1,5 +1,6 @@
 export default {
   title: "Kit de Interpolação para o QGIS",
+  style: "./custom.css"
   logo: "",  // Caminho para seu logo (deixe vazio se não quiser)
   footer: "Documentação técnica mantida por Vinicius Mendes.",
   favicon: "",  // Caminho para ícone (opcional)
